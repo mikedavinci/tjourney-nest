c68d001d27e7bdda017486c287baa3db643f12be
Miguel Esparza Jr <mike@mikedavinci.com>
2024-02-04 04:07:54 -0500

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiExcludeController, ApiTags } from '@nestjs/swagger';
 // import { GoogleAuthGuard } from './auth/guards/http-google-oath.guard';
 @ApiBearerAuth()
 @ApiTags('Hello')
+@ApiExcludeController()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
