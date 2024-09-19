52382c70b2f86371df608d1cc8e2c81a83d73f56
Miguel Esparza Jr <mike@mikedavinci.com>
2024-09-18 23:28:31 -0500

    UP

--- src/alerts/alerts.controller.ts
+++ src/alerts/alerts.controller.ts
@@ -59,7 +59,7 @@ export class AlertController {
   async createStocksAlert(
     @Body(ValidationPipe) createAlertDto: CreateAlertDto
   ): Promise<Alert> {
-    return await this.alertService.saveStocksAlertData(createAlertDto);
+    return await this.alertService.saveAlertData(createAlertDto);
   }
 
   @Post('create-alert-forex')
@@ -73,7 +73,7 @@ export class AlertController {
   async createForexAlert(
     @Body(ValidationPipe) createAlertDto: CreateAlertDto
   ): Promise<Alert> {
-    return await this.alertService.saveForexAlertData(createAlertDto);
+    return await this.alertService.saveAlertData(createAlertDto);
   }
 
   @Get()
